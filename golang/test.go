87d3d9525c1a72ab2e6ae9058ceb084b8efc8e87
yuan <1212@121.com>
2022-07-09 15:08:04 +0800

    sdsd

--- golang/test.go
+++ golang/test.go
@@ -15,7 +15,27 @@ func main() {
 	shortUrl := murmur3.Sum64([]byte(domain))
 	shortUrl1 := murmur3.Sum32([]byte(domain1))
 
+	nick := Person{
+		Name:  "nick",
+		Age:   28,
+		Email: "nickli@xxx.com",
+	}
+
 	fmt.Println(shortUrl1)
 	fmt.Printf("shortUr   is %d, length is %d\n", shortUrl, utf8.RuneCountInString(strconv.Itoa(int(shortUrl))))
 	fmt.Printf("shortUrl  is %d, length is %d\n", shortUrl1, utf8.RuneCountInString(strconv.Itoa(int(shortUrl1))))
+
+	fmt.Println(nick.Name)
+	fmt.Println(LearnStruct())
+}
+
+func LearnStruct() int {
+	fmt.Println("LearnStruct ===> ")
+	return 1
+}
+
+type Person struct {
+	Name  string
+	Age   int
+	Email string
 }
